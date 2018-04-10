da4946c2f18f2f1226d6fd46ad2c2b9d0edc64a8
zpltys <zpltys@163.com>
2018-04-10 11:06:10 +0800

    test

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include "../include/parmetis.h"
+#include <cstdio>
+
+int main (int argc, char *argv[])
+//      int argc;
+//      char *argv[];
+{
+    int myid, numprocs, namelen;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init (&argc, &argv);        /* starts MPI */
+    MPI_Comm_rank (MPI_COMM_WORLD, &myid);  /* get current process id */
+    MPI_Comm_size (MPI_COMM_WORLD, &numprocs);      /* get number of processes */
+    MPI_Get_processor_name(processor_name,&namelen);
+
+    if(myid == 0) printf("number of processes: %d\n",numprocs);
+    printf( "%s: Hello world from process %d \n", processor_name, myid);
+
+    MPI_Finalize();
+    return 0;
+}
