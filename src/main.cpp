217c7df5929b0065e10f39436de3b90b565655a0
zpltys <zpltys@163.com>
2018-04-11 16:37:36 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 #include <vector>
 using namespace std;
 
-char* prePath = "/mnt/nfs/zpltys/tempDir/";
+const char* prePath = "/mnt/nfs/zpltys/tempDir/";
+const int vertexNum = 1024;
 
 struct edge {
     idx_t x, y;
@@ -23,7 +24,7 @@ bool cmp(const edge& a, const edge& b) {
 int main (int argc, char *argv[])
 {
     int myid, numprocs, namelen;
-    int i;
+    int i, j;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
 
     MPI_Init (&argc, &argv);        /* starts MPI */
@@ -52,6 +53,53 @@ int main (int argc, char *argv[])
         }
     }
     delete[] path;
+
+    idx_t *vtxdist, *xadj, *adjcny, *wgtflag, *numflag, *ncon, *nparts, *options, *edgecut, *part;
+    real_t *tpwgts, *ubvec;
+    vtxdist = new idx_t[numprocs + 1];
+    xadj = new idx_t[vertexNum / numprocs + 1];
+    adjcny = new idx_t[e.size() + 1];
+
+    for (i = 0; i <= numprocs; i++) vtxdist[i] = vertexNum / numprocs * i;
+    idx_t bx = vtxdist[myid];
+    i = 0; j = 0;
+    adjcny[j++] = 0;
+    for (vector<edge>::iterator it = e.begin(); it != e.end(); it++) {
+        x = it->x;
+        y = it->y;
+        if (bx == x) {
+            if (y == adjcny[i - 1]) continue;
+            adjcny[i++] = y;
+        } else {
+            while (bx < x) {
+                bx++;
+                xadj[j++] = i;
+            }
+            adjcny[i++] = y;
+        }
+    }
+    while (bx < vtxdist[myid + 1]) {
+        bx++;
+        xadj[j++] = i;
+    }
+
+    wgtflag = new idx_t(0);
+    numflag = new idx_t(0);
+    ncon = new idx_t(1);
+    nparts = new idx_t(4);
+
+    tpwgts = new real_t[1 * 4];
+    ubvec = new real_t[4];
+    for (i = 0; i < 1 * 4; i++) {
+        tpwgts[i] = 1.0 / 4;
+        ubvec[i] = 1.05;
+    }
+    options = new idx_t[5];
+    options[0] = 0;
+    edgecut = new idx_t(0);
+    part = new idx_t[vertexNum / numprocs];
+
+    ParMETIS_V3_PartKway(vtxdist, xadj, adjcny, NULL, NULL, wgtflag, numflag, ncon, nparts, tpwgts, ubvec, options, edgecut, part, MPI_COMM_WORLD);
     MPI_Finalize();
     return 0;
 }
